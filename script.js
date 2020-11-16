3d8636094303512a3155d05b3e2927149cdf99d8
Amara Jaeger <amara@amaras-air.home>
2020-11-16 17:43:01 -0500

    initial commit, pixelated webcam detects motion

---
+++ script.js
@@ -0,0 +1,51 @@
+const w = 1280
+const h = 720
+const sample_size = 15
+const threshold = 100;
+let previous_frame = [];
+
+const draw = (vid) => {
+    ctx.drawImage(vid, 0, 0, w, h)
+    const data = ctx.getImageData(0, 0, w, h).data;
+    // for rows and columns in pixel array:
+    for (let y = 0; y < h; y += sample_size) {
+        for (let x = 0; x < w; x += sample_size) {
+            // the data array is a continuous array of red, blue, green and alpha values, so each pixel takes up four values in the array
+            let pos = (x + y * w) * 4;
+
+            // get red, blue and green pixel value
+            let r = data[pos];
+            let g = data[pos + 1];
+            let b = data[pos + 2];
+
+            if (previous_frame[pos] && Math.abs(previous_frame[pos] - r) > threshold) {
+                // draw the pixels as blocks of colours
+                ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+                ctx.fillRect(x, y, sample_size, sample_size);
+                previous_frame[pos] = r;
+                console.log('movement')
+
+            } else {
+                ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+                ctx.fillRect(x, y, sample_size, sample_size);
+                previous_frame[pos] = r;
+            }
+        }
+    }
+}
+
+var video = document.querySelector('video')
+const options = {
+    video: { width: 1280, height: 720 }
+}
+navigator.mediaDevices.getUserMedia(options)
+    .then(function (stream) {
+        video.srcObject = stream
+        setInterval(() => draw(video), 100)
+    })
+    .catch((error) => {
+        console.log("An error occured")
+    })
+
+const canvas = document.querySelector("#canvas")
+const ctx = canvas.getContext("2d")
