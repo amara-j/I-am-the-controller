1420f076d7e2880ecb9d060a465be6403cabe08e
Amara Jaeger <amara@amaras-air.home>
2020-11-17 13:10:15 -0500

    colors areas where movement is detected and resizes with browser window

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const w = 1280
-const h = 720
+var win = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0], x = win.innerWidth || e.clientWidth || g.clientWidth, y = win.innerHeight || e.clientHeight || g.clientHeight;
+var w = x
+var h = y
+
 const sample_size = 15
-const threshold = 100;
+const threshold = 90;
 let previous_frame = [];
 
-console.log(Tone)
+var onresize = function (e) {
+    w = e.target.outerWidth;
+    h = e.target.outerHeight;
+}
+window.addEventListener("resize", onresize);
 
 const synth = new Tone.MonoSynth().toDestination()
 
@@ -28,15 +34,23 @@ const draw = (vid) => {
             let g = data[pos + 1];
             let b = data[pos + 2];
 
-            if (previous_frame[pos] && Math.abs(previous_frame[pos] - r) > threshold) {
+            if (previous_frame[pos] && Math.abs(previous_frame[pos] - g) > threshold) {
                 // draw the pixels as blocks of colours
+                console.log("pos=", pos)
+                console.log("r= ", data[pos])
+                console.log("g= ", data[pos + 1])
+                console.log("b= ", data[pos + 2])
+                r = Math.random() * 255
+                g = Math.random() * 255
+                b = Math.random() * 255
                 ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
                 ctx.fillRect(x, y, sample_size, sample_size);
                 previous_frame[pos] = r;
                 console.log('movement')
                 playSynth()
+            }
 
-            } else {
+            else {
                 ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
                 ctx.fillRect(x, y, sample_size, sample_size);
                 previous_frame[pos] = r;
