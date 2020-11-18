4fb0aded0a9abd43091c2226d77c65c21a3f8ec4
Amara Jaeger <amara@amaras-air.home>
2020-11-18 18:18:43 -0500

    we kinda broke it but it will be better soon... trying to get CPU% down

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 var win = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0], x = win.innerWidth || e.clientWidth || g.clientWidth, y = win.innerHeight || e.clientHeight || g.clientHeight;
-var w = x
-var h = y
+var w = document.getElementById("small_canvas").getAttribute("width");
+var h = document.getElementById("small_canvas").getAttribute("height");
 
-const sample_size = 15
-const threshold = 90;
-let previous_frame = [];
+//make a tiny invisible canvas to later scale up for pixelating effect
+// const offscreen_postage_stamp = new OffscreenCanvas(128,72);
+// offscreen_postage_stamp.getContext('2d').rect(0,0,20,20);
+
+
+const canvasContainer = document.querySelector("#canvas-container")
+const postage_stamp = document.createElement('canvas')
+
+
+
+postage_stamp.offscreenCanvas = document.createElement('canvas')
+postage_stamp.offscreenCanvas.width = 128
+postage_stamp.offscreenCanvas.height = 72
+const ctx1 = postage_stamp.offscreenCanvas.getContext('2d')
+ctx1.beginPath()
+ctx1.rect(0,0,20,20)
+ctx1.stroke()
+postage_stamp.getContext('2d').drawImage(postage_stamp.offscreenCanvas, 0,0)
+
+canvasContainer.appendChild(postage_stamp);
+postage_stamp.width = 128
+postage_stamp.height = 72
+// postage_stamp.getContext('2d').drawImage(offscreen_postage_stamp, 0,0);
 
 var onresize = function (e) {
     w = e.target.outerWidth;
     h = e.target.outerHeight;
 }
-window.addEventListener("resize", onresize);
+//window.addEventListener("resize", onresize);
 
-const synth = new Tone.MonoSynth().toDestination()
+const volume = new Tone.Volume(-12).toDestination()
+const synth = new Tone.PluckSynth().connect(volume)
 
 const playSynth = () => {
     //fr 100 440
     const fr = Math.floor(Math.random() * 340) + 100
-    synth.triggerAttackRelease(fr, "8n")
+    synth.triggerAttackRelease(fr, 0.5)
+    // setTimeout(() => synth.dispose(), 500)
 }
 
-const draw = (vid) => {
+const sample_size = 1
+const threshold = 90;
+let previous_frame = [];
+
+//creating an offscreen canvas
+//update function outside of loop
+// separate draw and update functions
+
+const draw = (timestamp, vid) => {
     ctx.drawImage(vid, 0, 0, w, h)
     const data = ctx.getImageData(0, 0, w, h).data;
     // for rows and columns in pixel array:
@@ -30,24 +60,26 @@ const draw = (vid) => {
             let pos = (x + y * w) * 4;
 
             // get red, blue and green pixel value
+
+            // copy imagedata 
+            // modify new imagedata that isn't on canvas
+            // then draw that on canvas after update
+
             let r = data[pos];
             let g = data[pos + 1];
             let b = data[pos + 2];
 
             if (previous_frame[pos] && Math.abs(previous_frame[pos] - g) > threshold) {
                 // draw the pixels as blocks of colours
-                console.log("pos=", pos)
-                console.log("r= ", data[pos])
-                console.log("g= ", data[pos + 1])
-                console.log("b= ", data[pos + 2])
                 r = Math.random() * 255
                 g = Math.random() * 255
                 b = Math.random() * 255
                 ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
                 ctx.fillRect(x, y, sample_size, sample_size);
                 previous_frame[pos] = r;
-                console.log('movement')
-                playSynth()
+                // console.log('movement')
+                //if there's movement, only fire this event every 50ms
+                // playSynth()
             }
 
             else {
@@ -57,19 +89,22 @@ const draw = (vid) => {
             }
         }
     }
+    window.requestAnimationFrame((timestamp) => draw(timestamp, vid))
 }
+
 var video = document.querySelector('video')
 const options = {
-    video: { width: 1280, height: 720 }
+    video: { width: w, height: h }
 }
 navigator.mediaDevices.getUserMedia(options)
-    .then(function (stream) {
+    .then(function (stream) {  // Main function
         video.srcObject = stream
-        setInterval(() => draw(video), 10)
+        Tone.start()
+        // window.requestAnimationFrame((timestamp) => draw(timestamp, video))
     })
     .catch((error) => {
         console.log("An error occured")
     })
 
-const canvas = document.querySelector("#canvas")
-const ctx = canvas.getContext("2d")
+const small_canvas = document.querySelector("#small_canvas")
+const ctx = small_canvas.getContext("2d")
